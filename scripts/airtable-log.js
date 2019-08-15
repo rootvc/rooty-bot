17f8d43d68e894d0fba7e2f8aa7af4d887d4adfc
Adam <adam.eldefrawy@gmail.com>
2019-08-15 15:41:31 -0700

    webdriver

--- scripts/airtable-log.js
+++ scripts/airtable-log.js
@@ -52,8 +52,8 @@ module.exports = function (robot) {
     robot.respond(/whois (.*)/i, function(msg){
       company = functions.getCompanyNameFromMsg(msg);
       functions.whoisCrunchbaseOneCompany(company).then(function (result){
-        console.log(result);
-        msg.reply(result[0]);
+        console.log(result.toString());
+        msg.reply(result.stringify());
       });
     });
 
